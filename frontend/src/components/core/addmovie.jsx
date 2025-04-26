a96cb92a71c871d3f0357f695060490d9030603c
Abi <97228376+abijanu101@users.noreply.github.com>
2025-04-27 01:58:02 +0500

    /m/create page added
    Image Preview and shit, cool stuff

--- frontend/src/components/core/addmovie.jsx
+++ frontend/src/components/core/addmovie.jsx
@@ -1,6 +1,88 @@
-function AddMovie () {
+import { useRef, useState } from "react";
+import ButtonFilled from "../commons/buttonFilled";
+import { useNavigate } from "react-router-dom";
 
+function AddMovie() {
+    // form values
+    const title = useRef();
+    const [file, setFile] = useState();
+    const date = useRef();
+    const synopsis = useRef();
 
+    // view stuff
+    const [image, setImage] = useState();
+    const [feedback, setFeedback] = useState(null);
+
+    const navigate = useNavigate();
+
+    // handlers
+
+    function handleFileUpload(event) {
+        setFile(event.target.files[0]);
+        setImage(URL.createObjectURL(event.target.files[0]));
+    }
+
+    function handleSubmission() {
+        if (!title.current.value || !file || !date.current.value || !synopsis.current.value)
+            setFeedback("Must input all fields!");
+        else {
+            setFeedback(null);
+
+            const formData = new FormData();
+            formData.append('title', title.current.value);
+            formData.append('image', file);
+            formData.append('release_date', date.current.value);
+            formData.append('synopsis', synopsis.current.value);
+
+            // post here
+
+            navigate('/m/');
+        }
+    }
+
+    // component
+
+    return (
+        <div className="p-5">
+            <h1 className="text-3xl flex-1 p-2 border-b border-green-800">Add a Movie</h1>
+
+            <div className="m-5 flex flex-col gap-3">
+                <div className="flex gap-6">
+                    <input type="text" placeholder="Movie Title" className="p-2 border-green-800 border-2 rounded-md flex-1" ref={title} />
+                    <div className="flex gap-6">
+                        {
+                            file ?
+                                <label htmlFor="mv_img" className="p-2 px-5 border-2 border-green-800 rounded-md hover:text-green-800 hover:from-teal-800/0 hover:to-green-800/0 bg-gradient-to-r from-teal-800 text-white to-green-700 transition-colors duration-300">Replace Image</label>
+                                :
+                                <label htmlFor="mv_img" className="p-2 px-5 border-2 border-green-800 rounded-md hover:text-green-800 hover:from-teal-800/0 hover:to-green-800/0 bg-gradient-to-r from-teal-800 text-white to-green-700 transition-colors duration-300">Upload an Image</label>
+                        }
+                        <input id="mv_img" type="file" className="hidden" accept="image/*" onChange={handleFileUpload} />
+                    </div>
+                    <input type="date" placeholder="Date" className="p-2 border-green-800 border-2 rounded-md" ref={date} />
+                </div>
+                <div className="flex gap-5">
+                    <div className="flex-1 shrink-1">
+                        <textarea placeholder="Plot Synopsis" className="w-full h-25 p-2 border-green-800 border-2 rounded-md" ref={synopsis} />
+
+                    </div>
+                    <div className="w-70 h-105 bg-gray-800 shrink-0">
+                        <img src={image} className="w-full h-full" />
+                        <p className="text-center text-gray-500">Recommended Size: 1400x2100</p>
+                    </div>
+
+                </div>
+
+                {feedback &&
+                    <div className="p-3 bg-red-100 border-2 border-red-500/50 text-red-900/85">{feedback}</div>
+                }
+                <div className="w-full flex flex-row justify-center">
+                    <div onClick={handleSubmission} className="shadow-md shadow-green-800/50 active:brightness-110 p-2 px-5 border-2 border-green-800 rounded-md hover:text-green-800 hover:from-teal-800/0 hover:to-green-800/0 bg-gradient-to-r from-teal-800 text-white to-green-700 transition-colors duration-300">
+                        Submit
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default AddMovie;
--- frontend/src/components/essentials/notfound.jsx
+++ frontend/src/components/essentials/notfound.jsx
@@ -1,6 +1,22 @@
-function NotFound () {
-    return <p>NOT FOUND</p>
-
+function NotFound() {
+    return (
+        <div className="flex text-center m-10">
+            <div className="w-40"></div>
+            <div className="flex-1 font-serif">
+                <h1 className="text-5xl font-bold p-5 border-b-3 text-gray-800 border-green-800/50">Webpage Not Found :&#40;</h1>
+                <div className="flex justify-center pt-20">
+                    <h2 style={{ 'font-size': '20rem' }}>
+                        <BsCameraReelsFill className="block m-0 text-teal-800/20" />
+                        <BsCameraReels className="block m-0 text-green-800/50 -mt-80" />
+                        <p style={{ 'font-size': '25rem' }} className="block m-0 -mt-115 rotate-25 text-gray-800">/</p>
+                    </h2>
+                </div>
+                <p className='mb-10 text-2xl'>There are still many more movies to explore!</p>
+                <ButtonFilled text="Return Home" dest="/home/" />
+            </div>
+            <div className="w-40"></div>
+        </div>
+    );
 }
 
 export default NotFound;
